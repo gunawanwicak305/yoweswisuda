2dd7f93796f0c312924b3e3d993dfc561c2e1e01
Gunawan Wicaksono <gunawanwicak305@gmail.com>
2026-05-12 04:09:54 +0700

    fix page props

--- app/gallery/[code]/page.tsx
+++ app/gallery/[code]/page.tsx
@@ -8,14 +8,12 @@ export default async function GalleryPage({
 
   const { code } = await params
 
-  // participant
   const { data: participant } = await supabase
     .from("participants")
     .select("*")
     .eq("participant_code", code)
     .single()
 
-  // photos
   const { data: photos } = await supabase
     .from("photos")
     .select("*")
@@ -48,27 +46,31 @@ export default async function GalleryPage({
 
         {photos?.map((photo) => (
 
-         <div
-  key={photo.id}
-  className="bg-white rounded-3xl overflow-hidden"
->
+          <div
+            key={photo.id}
+            className="bg-white rounded-3xl overflow-hidden"
+          >
 
-  <img
-    src={photo.image_url}
-    alt="wisuda"
-    className="rounded-t-3xl w-full h-[400px] object-cover"
-  />
+            <img
+              src={photo.image_url}
+              alt="wisuda"
+              className="rounded-t-3xl w-full h-[400px] object-cover"
+            />
 
-  <a
-    href={photo.image_url}
-    download
-    target="_blank"
-    className="block text-center bg-black text-white py-4 font-bold hover:bg-zinc-800"
-  >
-    Download Foto
-  </a>
+            <a
+              href={photo.image_url}
+              download
+              target="_blank"
+              className="block text-center bg-black text-white py-4 font-bold hover:bg-zinc-800"
+            >
+              Download Foto
+            </a>
 
-</div>
+          </div>
+
+        ))}
+
+      </div>
 
     </main>
 
