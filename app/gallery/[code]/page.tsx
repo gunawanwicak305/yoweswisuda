f59d4589a44b012c24d4abed342f787097b537ed
Gunawan Wicaksono <gunawanwicak305@gmail.com>
2026-05-12 03:53:08 +0700

    fix next15 params

--- app/gallery/[code]/page.tsx
+++ app/gallery/[code]/page.tsx
@@ -1,82 +1,65 @@
 import { supabase } from "../../../lib/supabase"
 
 type Props = {
-  params: {
+  params: Promise<{
     code: string
-  }
+  }>
 }
 
 export default async function GalleryPage({
   params
 }: Props) {
 
+  const { code } = await params
+
+  // ambil participant
   const { data: participant } = await supabase
     .from("participants")
     .select("*")
-    .eq("participant_code", params.code)
+    .eq("participant_code", code)
     .single()
 
+  // ambil photos
   const { data: photos } = await supabase
     .from("photos")
     .select("*")
-    .eq("participant_code", params.code)
+    .eq("participant_code", code)
 
   if (!participant) {
 
     return (
-      <main className="p-10 text-white bg-black min-h-screen">
-
+      <main className="p-10">
         <h1 className="text-3xl font-bold">
           Peserta tidak ditemukan
         </h1>
-
       </main>
     )
   }
 
   return (
 
-    <main className="min-h-screen bg-black text-white p-10">
+    <main className="min-h-screen bg-gray-100 p-10">
 
-      <div className="max-w-7xl mx-auto">
+      <h1 className="text-4xl font-bold">
+        {participant.full_name}
+      </h1>
 
-        <h1 className="text-5xl font-bold">
-          {participant.full_name}
-        </h1>
+      <p className="mt-2 text-gray-600">
+        Kode: {participant.participant_code}
+      </p>
 
-        <p className="mt-3 text-gray-400 text-lg">
-          Kode: {participant.participant_code}
-        </p>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-10">
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-14">
+        {photos?.map((photo) => (
 
-          {photos?.map((photo) => (
+          <img
+            key={photo.id}
+            src={photo.image_url}
+            alt="wisuda"
+            className="rounded-2xl w-full h-[400px] object-cover"
+          />
 
-            <div
-              key={photo.id}
-              className="overflow-hidden rounded-3xl bg-white"
-            >
-
-              <img
-                src={photo.image_url}
-                alt="wisuda"
-                className="w-full h-[400px] object-cover hover:scale-105 duration-300"
-              />
-
-              <a
-                href={photo.image_url}
-                download
-                target="_blank"
-                className="block bg-white text-black text-center py-3 font-semibold"
-              >
-                Download
-              </a>
-
-            </div>
-
-          ))}
-
-        </div>
+        ))}
 
       </div>
 
